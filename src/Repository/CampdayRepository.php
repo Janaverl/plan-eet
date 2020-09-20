1cdb2f6702da22387eca05f409f3427649a9c8a4
Janaverl <36476740+Janaverl@users.noreply.github.com>
2020-09-20 12:37:57 +0200

    Update: doctrine/common-bundle to v. 3.0

--- src/Repository/CampdayRepository.php
+++ src/Repository/CampdayRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Campday;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Campday|null find($id, $lockMode = null, $lockVersion = null)
