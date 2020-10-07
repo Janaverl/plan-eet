094d948b8bf4f02ab95152c925806cee717d46ee
Janaverl <36476740+Janaverl@users.noreply.github.com>
2020-10-07 15:31:30 +0200

    Add: API for camp supplies and ingredient queries

---
+++ src/Controller/Api/SuppliesApiController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller\Api;
+
+
+use App\Entity\Camp;
+use App\Entity\Ingredient;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class SuppliesApiController extends ApiController
+{
+    public function show_for_camp(Camp $camp, Request $request)
+    {
+        $this->throwExceptionIfNotExcists($camp);
+        $this->denyAccessUnlessGranted('view', $camp);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if (empty($request->query->get('rayons')) || empty($request->query->get('daycount'))) {
+            $allIngredients = $entityManager->getRepository(Ingredient::class)
+                ->findArrayByCamp($camp);
+        } else {
+            $rayon = $request->query->get('rayons');
+            $daycount = $request->query->get('daycount');
+            $allIngredients = $entityManager->getRepository(Ingredient::class)
+                ->findArrayByCampFilterByRayonsAndCampdays($camp, $rayon, $daycount);
+        }
+
+        return new JsonResponse($allIngredients);
+    }
+
+    public function show_for_camp_with_mealdetail(Camp $camp)
+    {
+        $this->throwExceptionIfNotExcists($camp);
+        $this->denyAccessUnlessGranted('view', $camp);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $allIngredients = $entityManager->getRepository(Ingredient::class)
+            ->findArraywithDatesByCamp($camp);
+
+        $json = $this->mapByRayon($allIngredients);
+
+        return new JsonResponse($json);
+    }
+
+    private function mapByRayon($array)
+    {
+        $json = [];
+
+        foreach ($array as $mealIngredient) {
+            // if rayon doesn't exist, create it
+            if (empty($json[$mealIngredient['rayon']]['rayonname'])) {
+                $json[$mealIngredient['rayon']]['rayonname'] = $mealIngredient['rayon'];
+            }
+
+            // if ingredient doesn't exist, create it
+            if (empty($json[$mealIngredient['rayon']]['supplies'][$mealIngredient['name']])) {
+                $setKeys = ['name', 'rayon'];
+                $subarray = $this->mapKeysInArray($mealIngredient, $setKeys);
+
+                $json[$mealIngredient['rayon']]['supplies'][$mealIngredient['name']] = $subarray;
+            }
+            
+            // set the keys for the subarray
+            $setKeys = ['quantity', 'unit', 'moment', 'campdaycount', 'date'];
+            $subarray = $this->mapKeysInArray($mealIngredient, $setKeys);
+
+            $json[$mealIngredient['rayon']]['supplies'][$mealIngredient['name']]['meals'][] = $subarray;
+        };
+
+        return $json;
+    }
+
+    /**
+     * Returns a mapped array with values from a given array
+     * For the keys and values that are asked
+     *
+     * @param array $arr
+     * @param array $keys
+     * @return void
+     */
+    private function mapKeysInArray($arr, $keys)
+    {
+        $array = [];
+
+        foreach ($keys as $key) {
+            $array[$key] = $arr[$key];
+        }
+
+        return $array;
+    }
+}
--- src/Repository/IngredientRepository.php
+++ src/Repository/IngredientRepository.php
@@ -76,7 +76,61 @@ class IngredientRepository extends ServiceEntityRepository
             ->setParameter('count', $campdaycount);
 
         return $query->getResult();
+    }
 
+    public function findArraywithDatesByCamp(Camp $camp)
+    {
+        $entityManager = $this->getEntityManager();
+
+        $rsm = new ResultSetMapping();
+
+        $rsm
+            ->addScalarResult('ingredient', 'name')
+            ->addScalarResult('quant', 'quantity')
+            ->addScalarResult('campdaycount', 'campdaycount')
+            ->addScalarResult('meal_date', 'date')
+            ->addScalarResult('unit_name', 'unit')
+            ->addScalarResult('moment_name', 'moment')
+            ->addScalarResult('rayon_name', 'rayon');
+
+        $sql =
+            "SELECT
+                campday.campdaycount AS campdaycount,
+                ingredient.name AS ingredient,
+                SUM(ROUND((recipe_ingredients.quantity*(SELECT nr_of_participants FROM camp WHERE camp.id = :id)), 3)) AS quant,
+                unit.name AS unit_name,
+                rayon.name AS rayon_name,
+                mealmoment.name AS moment_name,
+                DATE_FORMAT(DATE_ADD(camp.start_time, INTERVAL (campday.campdaycount) DAY), '%d/%m/%Y') AS meal_date 
+
+            FROM campmeal
+
+            INNER JOIN mealcourse ON campmeal.id = mealcourse.campmeal_id
+            INNER JOIN recipes ON mealcourse.recipe_id = recipes.id
+            INNER JOIN recipe_ingredients ON recipes.id = recipe_ingredients.recipe_id
+            INNER JOIN ingredient ON recipe_ingredients.ingredient_id = ingredient.id
+            INNER JOIN unit ON ingredient.unit_id = unit.id
+            INNER JOIN rayon ON ingredient.rayon_id = rayon.id
+            INNER JOIN camp_mealmoments ON campmeal.camp_mealmoment_id = camp_mealmoments.id
+            INNER JOIN mealmoment ON camp_mealmoments.mealmoment_id = mealmoment.id
+            INNER JOIN campday ON campmeal.campday_id = campday.id
+            INNER JOIN camp ON camp.id = campday.camp_id
+
+            WHERE campmeal.id IN
+                (SELECT campmeal.id FROM campmeal WHERE camp_mealmoment_id IN
+                    (SELECT camp_mealmoments.id FROM camp_mealmoments
+                        WHERE camp_mealmoments.camp_id = :id
+                    )
+                )
+
+            GROUP BY ingredient, campdaycount";
+
+        $query = $entityManager->createNativeQuery($sql, $rsm);
+
+        $query
+            ->setParameter('id', $camp);
+
+        return $query->getResult();
     }
 
     public function findArrayByCamp(Camp $camp)
@@ -88,15 +142,15 @@ class IngredientRepository extends ServiceEntityRepository
         $rsm
             ->addScalarResult('ingredient', 'name')
             ->addScalarResult('quant', 'quantity')
-            ->addScalarResult('u_name', 'u_name')
-            ->addScalarResult('r_name', 'r_name');
+            ->addScalarResult('unit_name', 'unit')
+            ->addScalarResult('rayon_name', 'rayon');
 
         $sql =
             'SELECT
                 DISTINCT ingredient.name AS ingredient,
                 SUM(ROUND((recipe_ingredients.quantity*(SELECT nr_of_participants FROM camp WHERE camp.id = :id)), 3)) AS quant,
-                unit.name AS u_name,
-                rayon.name as r_name
+                unit.name AS unit_name,
+                rayon.name as rayon_name
 
             FROM campmeal
 
@@ -123,6 +177,112 @@ class IngredientRepository extends ServiceEntityRepository
             ->setParameter('id', $camp);
 
         return $query->getResult();
+    }
 
+    public function findArrayByCampFilterByRayon(Camp $camp, array $rayons)
+    {
+        $entityManager = $this->getEntityManager();
+
+        $rsm = new ResultSetMapping();
+
+        $rsm
+            ->addScalarResult('ingredient', 'name')
+            ->addScalarResult('quant', 'quantity')
+            ->addScalarResult('unit_name', 'unit')
+            ->addScalarResult('rayon_name', 'rayon');
+
+        $sql =
+            'SELECT
+                DISTINCT ingredient.name AS ingredient,
+                SUM(ROUND((recipe_ingredients.quantity*(SELECT nr_of_participants FROM camp WHERE camp.id = :id)), 3)) AS quant,
+                unit.name AS unit_name,
+                rayon.name as rayon_name
+
+            FROM campmeal
+
+            INNER JOIN mealcourse ON campmeal.id = mealcourse.campmeal_id
+            INNER JOIN recipes ON mealcourse.recipe_id = recipes.id
+            INNER JOIN recipe_ingredients ON recipes.id = recipe_ingredients.recipe_id
+            INNER JOIN ingredient ON recipe_ingredients.ingredient_id = ingredient.id
+            INNER JOIN unit ON ingredient.unit_id = unit.id
+            INNER JOIN rayon ON ingredient.rayon_id = rayon.id
+
+            WHERE campmeal.id IN
+                (SELECT campmeal.id FROM campmeal WHERE camp_mealmoment_id IN
+                    (SELECT camp_mealmoments.id FROM camp_mealmoments
+                        WHERE camp_mealmoments.camp_id = :id
+                    )
+                )
+            
+            AND (rayon.name IN (:rayons))
+
+            GROUP BY ingredient
+            ORDER BY rayon.name ASC';
+
+        $query = $entityManager->createNativeQuery($sql, $rsm);
+
+        $query
+            ->setParameter('id', $camp)
+            ->setParameter('rayons', $rayons);
+
+        return $query->getResult();
+    }
+
+    
+    public function findArrayByCampFilterByRayonsAndCampdays(Camp $camp, array $rayons, array $campdays)
+    {
+        $entityManager = $this->getEntityManager();
+
+        $rsm = new ResultSetMapping();
+
+        $rsm
+            ->addScalarResult('ingredient', 'name')
+            ->addScalarResult('quant', 'quantity')
+            ->addScalarResult('unit_name', 'unit')
+            ->addScalarResult('rayon_name', 'rayon');
+
+        $sql =
+            'SELECT
+                DISTINCT ingredient.name AS ingredient,
+                SUM(ROUND((recipe_ingredients.quantity*(SELECT nr_of_participants FROM camp WHERE camp.id = :id)), 3)) AS quant,
+                unit.name AS unit_name,
+                rayon.name as rayon_name
+
+            FROM campmeal
+
+            INNER JOIN mealcourse ON campmeal.id = mealcourse.campmeal_id
+            INNER JOIN recipes ON mealcourse.recipe_id = recipes.id
+            INNER JOIN recipe_ingredients ON recipes.id = recipe_ingredients.recipe_id
+            INNER JOIN ingredient ON recipe_ingredients.ingredient_id = ingredient.id
+            INNER JOIN unit ON ingredient.unit_id = unit.id
+            INNER JOIN rayon ON ingredient.rayon_id = rayon.id
+
+            WHERE campmeal.id IN
+                (SELECT campmeal.id FROM campmeal WHERE camp_mealmoment_id IN
+                    (SELECT camp_mealmoments.id FROM camp_mealmoments
+                        WHERE camp_mealmoments.camp_id = :id
+                    )
+                )
+            
+            AND (rayon.name IN (:rayons))
+
+            AND campmeal.campday_id IN
+                (SELECT campmeal.campday_id FROM campmeal WHERE campday_id IN
+                    (SELECT campday.id FROM campday
+                        WHERE campday.campdaycount IN (:campdays) 
+                    )
+                )
+
+            GROUP BY ingredient
+            ORDER BY rayon.name ASC';
+
+        $query = $entityManager->createNativeQuery($sql, $rsm);
+
+        $query
+            ->setParameter('id', $camp)
+            ->setParameter('rayons', $rayons)
+            ->setParameter('campdays', $campdays);
+
+        return $query->getResult();
     }
 }
